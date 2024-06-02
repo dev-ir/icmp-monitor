203c319e6072b774e9d42c3b0ff6af7c0c46fa7a
unknown <info@developermen.ir>
2024-06-02 23:09:51 +0330

    remvoe unused comment

--- icmp_monitoring.py
+++ icmp_monitoring.py
@@ -9,11 +9,9 @@ from bidi.algorithm import get_display
 import tkinter as tk
 from tkinter import simpledialog
 
-# تنظیم فونت پیش‌فرض برای matplotlib به Tahoma
 rcParams['font.family'] = 'Tahoma'
 rcParams['font.size'] = 12
 
-# تابع برای دریافت IP‌ها از کاربر با استفاده از یک پنجره گرافیکی
 def get_ips():
     root = tk.Tk()
     root.withdraw()  # مخفی کردن پنجره اصلی
@@ -23,13 +21,11 @@ def get_ips():
     else:
         return []
 
-# دریافت IP‌ها از کاربر
 ips = get_ips()
 if not ips:
     print("هیچ IP وارد نشد. برنامه خاتمه یافت.")
     exit(1)
 
-# تابع برای پینگ کردن IP‌ها
 def ping_ips():
     while True:
         responses = []
@@ -44,12 +40,10 @@ def ping_ips():
             ping_results.pop(0)
         time.sleep(1)
 
-# تنظیمات اولیه برای نمودار
 fig, ax = plt.subplots()
 ping_results = []
 max_display_time = 100  # حداکثر تعداد نقاط زمانی که در نمودار نمایش داده می‌شود
 
-# تابع برای به‌روزرسانی نمودار
 def update_graph(i):
     ax.clear()
     # استفاده از arabic_reshaper و bidi برای پشتیبانی از حروف فارسی و عربی
@@ -69,17 +63,13 @@ def update_graph(i):
     ax.set_xlim(left=max(0, len(ping_results) - max_display_time), right=len(ping_results))
     ax.set_ylim(-0.1, 1.1)
     
-    # تنظیم فونت و استفاده از arabic_reshaper و bidi برای برچسب‌ها
     labels = [get_display(arabic_reshaper.reshape(ip)) for ip in ips]
     ax.legend(labels=labels, prop={'family': 'Tahoma'}, loc='upper right')
 
-# شروع پینگ کردن IP‌ها در یک ترد جداگانه
 ping_thread = threading.Thread(target=ping_ips)
 ping_thread.daemon = True
 ping_thread.start()
 
-# شروع رسم نمودار به صورت پویا
 ani = animation.FuncAnimation(fig, update_graph, interval=500, save_count=100, cache_frame_data=False)
 
-# نمایش نمودار به صورت زنده
 plt.show()
